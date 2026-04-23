f35babe231b2a1fe4aa9984ab275221e3643d9da
nvnx7 <thenvnsahu@gmail.com>
2026-04-24 02:44:28 +0530

    chore: fix scripts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,8 @@
-import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
+  out: './common/drizzle',
+  schema: './common/src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL || 'postgres://postgres:admin123@localhost:5432/postgres',
